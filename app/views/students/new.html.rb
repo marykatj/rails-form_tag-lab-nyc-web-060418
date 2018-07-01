a81ac1954f589d0859143053c13d6c4073db6d8b
Mary-Kate James <marykatj@gmail.com>
2018-07-01 23:21:41 +0000

    automatically backed up by learn

--- app/views/students/new.html.rb
+++ app/views/students/new.html.rb
@@ -2,8 +2,8 @@
   <label>Student first_name:</label><br>
   <%= text_field_tag :'student[first_name]' %><br>
 
-  <label>Post description:</label><br>
-  <%= text_area_tag :'post[description]' %><br>
+  <label>Student last_name:</label><br>
+  <%= text_field_tag :'post[description]' %><br>
 
   <%= submit_tag "Submit Post" %>
 <% end %>
