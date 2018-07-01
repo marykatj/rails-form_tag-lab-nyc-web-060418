f4e77ca92f0e70085c72005e80bfc0c64a6cc099
Mary-Kate James <marykatj@gmail.com>
2018-07-01 23:43:10 +0000

    Done.

---
+++ app/views/students/new.html.rb
@@ -0,0 +1,11 @@
+# <%= form_tag students_path do %>
+#   <label>Student first_name:</label><br>
+#   <%= text_field_tag :'student[first_name]' %><br>
+#
+#   <label>Student last_name:</label><br>
+#   <%= text_field_tag :'student[last_name]' %><br>
+#
+#   <%= submit_tag "Submit Name" %>
+# <% end %>
+
+<%= render "form" %>
