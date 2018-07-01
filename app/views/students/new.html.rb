90bb1ae038897e1127f4d3820179b5e69d3ee6d2
Mary-Kate James <marykatj@gmail.com>
2018-07-01 23:27:18 +0000

    automatically backed up by learn

--- app/views/students/new.html.rb
+++ app/views/students/new.html.rb
@@ -1,9 +1,23 @@
+# <%= form_tag students_path do %>
+#   <label>Student first_name:</label><br>
+#   <%= text_field_tag :'student[first_name]' %><br>
+# 
+#   <label>Student last_name:</label><br>
+#   <%= text_field_tag :'student[last_name]' %><br>
+# 
+#   <%= submit_tag "Submit Name" %>
+# <% end %>
+
+<h3>Student Form</h3>
+
 <%= form_tag students_path do %>
-  <label>Student first_name:</label><br>
-  <%= text_field_tag :'student[first_name]' %><br>
+  <label>First Name:</label><br>
+  <%= text_field_tag :first_name %><br>
 
-  <label>Student last_name:</label><br>
-  <%= text_field_tag :'student[last_name]' %><br>
+  <label>Last Name:</label><br>
+  <%= text_field_tag :last_name %><br>
 
-  <%= submit_tag "Submit Name" %>
-<% end %>
+  <%= hidden_field_tag :authenticity_token, form_authenticity_token %>
+  
+  <%= submit_tag "Submit Student" %>
+<% end %
