e0fbc5b8b54a371d7444d9c36fef9577b47106e5
Mary-Kate James <marykatj@gmail.com>
2018-07-01 23:38:47 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
-
+  before_action :set_student, only: :show
+  
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -16,4 +16,9 @@ class StudentsController < ApplicationController
     redirect_to new_student_path
   end
 
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
